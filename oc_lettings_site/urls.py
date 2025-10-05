bba131811bed05cc5447d3b212255a8eb3bc6c15
dim-gggl <dimitri.gaggioli@gmail.com>
2025-10-05 18:36:16 +0200

    Migrations successfully processed + views refactoring

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -1,13 +1,15 @@
 from django.contrib import admin
 from django.urls import path
+import lettings.views as lettings_views
+import profiles.views as profiles_views
 
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('lettings/', views.lettings_index, name='lettings_index'),
-    path('lettings/<int:letting_id>/', views.letting, name='letting'),
-    path('profiles/', views.profiles_index, name='profiles_index'),
-    path('profiles/<str:username>/', views.profile, name='profile'),
+    path('lettings/', lettings_views.lettings_index, name='lettings_index'),
+    path('lettings/<int:letting_id>/', lettings_views.letting, name='letting'),
+    path('profiles/', profiles_views.profiles_index, name='profiles_index'),
+    path('profiles/<str:username>/', profiles_views.profile, name='profile'),
     path('admin/', admin.site.urls),
 ]
