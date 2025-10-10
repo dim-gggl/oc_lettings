d05c0c7ac4ce5898e64b283ee53f85f81a0410ee
dim-gggl <dimitri.gaggioli@gmail.com>
2025-10-10 17:21:15 +0200

    Ended the implementation of custom error pages

--- lettings/models.py
+++ lettings/models.py
@@ -34,7 +34,7 @@ class Address(models.Model):
     state = models.CharField(max_length=2,
                             validators=[MinLengthValidator(2)])
     zip_code = models.PositiveIntegerField(
-validators=[MaxValueValidator(99999)]
+        validators=[MaxValueValidator(99999)]
     )
     country_iso_code = models.CharField(max_length=3,
                                         validators=[MinLengthValidator(3)])
@@ -55,7 +55,7 @@ class Letting(models.Model):
         __str__: Returns the title of the letting.
     """
     title = models.CharField(max_length=256)
-    address = models.OneToOneField(Address, on_delete=models.CASCADE)
+    address = models.OneToOneField(to=Address, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -19,7 +19,6 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -34,6 +33,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
